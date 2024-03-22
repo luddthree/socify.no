277fe42293b8634c0f8a68739d79d8f41defac1d
luddthree~ <luddetv@gmail.com>
2024-03-22 15:09:53 +0100

    Remove unused code and dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,17 +7,26 @@ import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxtjs/tailwindcss', "@vueuse/nuxt", "@pinia/nuxt"
+    '@nuxtjs/tailwindcss', "@vueuse/nuxt", "@sidebase/nuxt-auth"
   ],
+  auth: {
+    provider: {
+        type: 'authjs'
+    }
+},
   
   ssr: false,
 
   runtimeConfig: {
+    public: {
+      gtagId: "G-Y9KX1S739M",
+    },
     OPENAI_API_KEY: "",
     databaseFilePath: env.DATABASE_PATH,
   },
 
   devtools: { enabled: true }
+  
 })
 
 
